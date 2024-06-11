62205eb512204ae93f141a08776c9e5a4b6eb673
Stephanie Tack <stephierathjen@gmx.de>
2024-06-11 21:56:13 +0200

    feature with javascript works

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.getElementById("toggleButton").addEventListener("click", function () {
-  var img = document.getElementById("buttonImage");
-  var currentSrc = img.getAttribute("src");
-  if (currentSrc === "assets/bookmark-white.svg") {
-    img.setAttribute("src", "assets/bookmark-black.svg");
-  } else {
-    img.setAttribute("src", "assets/bookmark-white.svg");
-  }
+document.querySelectorAll(".toggleButton").forEach((button) => {
+  button.addEventListener("click", function () {
+    var img = button.querySelector(".buttonImage");
+    var currentSrc = img.getAttribute("src");
+    if (currentSrc.endsWith("bookmark-white.svg")) {
+      img.setAttribute("src", "assets/bookmark-black.svg");
+    } else {
+      img.setAttribute("src", "assets/bookmark-white.svg");
+    }
+  });
 });
