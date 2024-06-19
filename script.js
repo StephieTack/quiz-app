c15944658d6d19dd8a6b1d83fb7c4a9b631a0479
Stephanie Tack <stephierathjen@gmx.de>
2024-06-19 13:09:54 +0200

    implementing darkmode functionality on profilepage

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ increaseByOneButton.addEventListener("click", () => {
   output.textContent = increaseByOneButton;
   console.log(increaseByOneButton);
 });
+
+const bodyElement = document.querySelector('[data-js="body"]');
+
+const toggleModeButton = document.querySelector('[data-js="toggle-button"]');
+toggleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
+console.log(toggleModeButton);
